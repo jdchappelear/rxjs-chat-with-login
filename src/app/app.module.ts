be19f9b18b591b8d3017c01f43b64409eceb4f77
John Chappelear <john.chappelear@gmail.com>
2019-11-12 21:25:59 -0500

    Added dash and log-in components and some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
